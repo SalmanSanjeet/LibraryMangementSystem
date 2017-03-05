9c99cfd2eaf6466f23d350779b9400e74e67f9c0
Tao Xie <xietao26@gmail.com>
2017-03-05 22:19:46 +0800

    Add win form member master.

---
+++ LibraryManagementSystem/LibraryManagementSystem/Member_Master.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class Member_Master
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.useridLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.mobileLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.dobLabel = new System.Windows.Forms.Label();
+            this.dojLabel = new System.Windows.Forms.Label();
+            this.planNameLabel = new System.Windows.Forms.Label();
+            this.planIDLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.usernameTextbox = new System.Windows.Forms.TextBox();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.emailTextbox = new System.Windows.Forms.TextBox();
+            this.mobileTextbox = new System.Windows.Forms.TextBox();
+            this.addressTextbox = new System.Windows.Forms.TextBox();
+            this.dobDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dojDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.planNameCombobox = new System.Windows.Forms.ComboBox();
+            this.planIDCombobox = new System.Windows.Forms.ComboBox();
+            this.statusCombobox = new System.Windows.Forms.ComboBox();
+            this.newButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.mmDataGridView = new System.Windows.Forms.DataGridView();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mmDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // useridLabel
+            // 
+            this.useridLabel.AutoSize = true;
+            this.useridLabel.Location = new System.Drawing.Point(35, 30);
+            this.useridLabel.Name = "useridLabel";
+            this.useridLabel.Size = new System.Drawing.Size(53, 12);
+            this.useridLabel.TabIndex = 0;
+            this.useridLabel.Text = "User ID:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(53, 57);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 12);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name:";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(47, 88);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(41, 12);
+            this.emailLabel.TabIndex = 2;
+            this.emailLabel.Text = "Email:";
+            // 
+            // mobileLabel
+            // 
+            this.mobileLabel.AutoSize = true;
+            this.mobileLabel.Location = new System.Drawing.Point(41, 118);
+            this.mobileLabel.Name = "mobileLabel";
+            this.mobileLabel.Size = new System.Drawing.Size(47, 12);
+            this.mobileLabel.TabIndex = 3;
+            this.mobileLabel.Text = "Mobile:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(35, 147);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(53, 12);
+            this.addressLabel.TabIndex = 4;
+            this.addressLabel.Text = "Address:";
+            // 
+            // dobLabel
+            // 
+            this.dobLabel.AutoSize = true;
+            this.dobLabel.Location = new System.Drawing.Point(376, 30);
+            this.dobLabel.Name = "dobLabel";
+            this.dobLabel.Size = new System.Drawing.Size(89, 12);
+            this.dobLabel.TabIndex = 5;
+            this.dobLabel.Text = "Date of Birth:";
+            // 
+            // dojLabel
+            // 
+            this.dojLabel.AutoSize = true;
+            this.dojLabel.Location = new System.Drawing.Point(382, 57);
+            this.dojLabel.Name = "dojLabel";
+            this.dojLabel.Size = new System.Drawing.Size(83, 12);
+            this.dojLabel.TabIndex = 6;
+            this.dojLabel.Text = "Date of Join:";
+            // 
+            // planNameLabel
+            // 
+            this.planNameLabel.AutoSize = true;
+            this.planNameLabel.Location = new System.Drawing.Point(400, 88);
+            this.planNameLabel.Name = "planNameLabel";
+            this.planNameLabel.Size = new System.Drawing.Size(65, 12);
+            this.planNameLabel.TabIndex = 7;
+            this.planNameLabel.Text = "Plan Name:";
+            // 
+            // planIDLabel
+            // 
+            this.planIDLabel.AutoSize = true;
+            this.planIDLabel.Location = new System.Drawing.Point(412, 118);
+            this.planIDLabel.Name = "planIDLabel";
+            this.planIDLabel.Size = new System.Drawing.Size(53, 12);
+            this.planIDLabel.TabIndex = 8;
+            this.planIDLabel.Text = "Plan ID:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(418, 147);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(47, 12);
+            this.statusLabel.TabIndex = 9;
+            this.statusLabel.Text = "Status:";
+            // 
+            // usernameTextbox
+            // 
+            this.usernameTextbox.Location = new System.Drawing.Point(105, 27);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(216, 21);
+            this.usernameTextbox.TabIndex = 10;
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(105, 57);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(216, 21);
+            this.nameTextbox.TabIndex = 11;
+            // 
+            // emailTextbox
+            // 
+            this.emailTextbox.Location = new System.Drawing.Point(105, 85);
+            this.emailTextbox.Name = "emailTextbox";
+            this.emailTextbox.Size = new System.Drawing.Size(216, 21);
+            this.emailTextbox.TabIndex = 12;
+            // 
+            // mobileTextbox
+            // 
+            this.mobileTextbox.Location = new System.Drawing.Point(105, 115);
+            this.mobileTextbox.Name = "mobileTextbox";
+            this.mobileTextbox.Size = new System.Drawing.Size(216, 21);
+            this.mobileTextbox.TabIndex = 13;
+            // 
+            // addressTextbox
+            // 
+            this.addressTextbox.Location = new System.Drawing.Point(105, 144);
+            this.addressTextbox.Multiline = true;
+            this.addressTextbox.Name = "addressTextbox";
+            this.addressTextbox.Size = new System.Drawing.Size(216, 92);
+            this.addressTextbox.TabIndex = 14;
+            // 
+            // dobDateTimePicker
+            // 
+            this.dobDateTimePicker.Location = new System.Drawing.Point(481, 24);
+            this.dobDateTimePicker.Name = "dobDateTimePicker";
+            this.dobDateTimePicker.Size = new System.Drawing.Size(206, 21);
+            this.dobDateTimePicker.TabIndex = 15;
+            // 
+            // dojDateTimePicker
+            // 
+            this.dojDateTimePicker.Location = new System.Drawing.Point(481, 54);
+            this.dojDateTimePicker.Name = "dojDateTimePicker";
+            this.dojDateTimePicker.Size = new System.Drawing.Size(206, 21);
+            this.dojDateTimePicker.TabIndex = 16;
+            // 
+            // planNameCombobox
+            // 
+            this.planNameCombobox.FormattingEnabled = true;
+            this.planNameCombobox.Location = new System.Drawing.Point(481, 85);
+            this.planNameCombobox.Name = "planNameCombobox";
+            this.planNameCombobox.Size = new System.Drawing.Size(206, 20);
+            this.planNameCombobox.TabIndex = 17;
+            // 
+            // planIDCombobox
+            // 
+            this.planIDCombobox.FormattingEnabled = true;
+            this.planIDCombobox.Location = new System.Drawing.Point(481, 115);
+            this.planIDCombobox.Name = "planIDCombobox";
+            this.planIDCombobox.Size = new System.Drawing.Size(206, 20);
+            this.planIDCombobox.TabIndex = 18;
+            // 
+            // statusCombobox
+            // 
+            this.statusCombobox.FormattingEnabled = true;
+            this.statusCombobox.Location = new System.Drawing.Point(481, 144);
+            this.statusCombobox.Name = "statusCombobox";
+            this.statusCombobox.Size = new System.Drawing.Size(206, 20);
+            this.statusCombobox.TabIndex = 19;
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(378, 186);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(72, 31);
+            this.newButton.TabIndex = 20;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(459, 186);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(72, 31);
+            this.updateButton.TabIndex = 21;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(540, 186);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(72, 31);
+            this.deleteButton.TabIndex = 22;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(621, 186);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(72, 31);
+            this.saveButton.TabIndex = 23;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // mmDataGridView
+            // 
+            this.mmDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mmDataGridView.Location = new System.Drawing.Point(37, 256);
+            this.mmDataGridView.Name = "mmDataGridView";
+            this.mmDataGridView.RowTemplate.Height = 23;
+            this.mmDataGridView.Size = new System.Drawing.Size(656, 246);
+            this.mmDataGridView.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(610, 510);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 12);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Row count: 0";
+            // 
+            // Member_Master
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 531);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.mmDataGridView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.statusCombobox);
+            this.Controls.Add(this.planIDCombobox);
+            this.Controls.Add(this.planNameCombobox);
+            this.Controls.Add(this.dojDateTimePicker);
+            this.Controls.Add(this.dobDateTimePicker);
+            this.Controls.Add(this.addressTextbox);
+            this.Controls.Add(this.mobileTextbox);
+            this.Controls.Add(this.emailTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.usernameTextbox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.planIDLabel);
+            this.Controls.Add(this.planNameLabel);
+            this.Controls.Add(this.dojLabel);
+            this.Controls.Add(this.dobLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.mobileLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.useridLabel);
+            this.Name = "Member_Master";
+            this.Text = "Member Master";
+            ((System.ComponentModel.ISupportInitialize)(this.mmDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label useridLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label mobileLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label dobLabel;
+        private System.Windows.Forms.Label dojLabel;
+        private System.Windows.Forms.Label planNameLabel;
+        private System.Windows.Forms.Label planIDLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.TextBox usernameTextbox;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.TextBox emailTextbox;
+        private System.Windows.Forms.TextBox mobileTextbox;
+        private System.Windows.Forms.TextBox addressTextbox;
+        private System.Windows.Forms.DateTimePicker dobDateTimePicker;
+        private System.Windows.Forms.DateTimePicker dojDateTimePicker;
+        private System.Windows.Forms.ComboBox planNameCombobox;
+        private System.Windows.Forms.ComboBox planIDCombobox;
+        private System.Windows.Forms.ComboBox statusCombobox;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView mmDataGridView;
+        private System.Windows.Forms.Label label11;
+    }
+}
