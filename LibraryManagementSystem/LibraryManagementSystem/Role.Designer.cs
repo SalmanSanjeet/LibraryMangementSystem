eb26eed5bbcc8ab138dedc1790b97ab6cf563c40
Tao Xie <xietao26@gmail.com>
2017-03-21 06:32:50 +0800

    Finish the feature of save new row.

--- LibraryManagementSystem/LibraryManagementSystem/Role.Designer.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Role.Designer.cs
@@ -120,6 +120,7 @@
             this.saveButton.TabIndex = 9;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // roleDataGridView
             // 
--- LibraryManagementSystem/LibraryManagementSystem/Role.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Role.cs
@@ -32,20 +32,36 @@ namespace LibraryManagementSystem
             CreateNew();
         }
 
-        void CreateNew()
+        private void CreateNew()
         {
             Connection conn = new Connection();
             MySqlDataAdapter mda = new MySqlDataAdapter("Proc_New_Role", conn.ActiveCon());
             mda.SelectCommand.CommandType = CommandType.StoredProcedure;
 
-            //DataTable dt = new DataTable();
-            //mda.Fill(dt);
+            // Handle multiple sql resutls
             DataSet ds = new DataSet();
             mda.Fill(ds);
-            //bookIDTextbox.Text = dt.Rows[0][0].ToString();
             DataTableCollection dc = ds.Tables;
             bookIDTextbox.Text = dc[dc.Count - 1].Rows[0][0].ToString();
+        }
 
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            AddRecord();
+        }
+
+        private void AddRecord()
+        {
+            Connection conn = new Connection();
+            MySqlCommand cmd = new MySqlCommand(@"insert into lmsdb.role_master
+            (role_id
+            ,role
+            ,role_status)
+            values
+            ('" + bookIDTextbox.Text +"', " + 
+            "'" + bookNameTextbox.Text + "', " + 
+            "'" + statusCombobox.Text + "')", conn.ActiveCon());
+            cmd.ExecuteNonQuery();
         }
     }
 }
